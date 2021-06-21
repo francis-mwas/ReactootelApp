0cc63f11a93d6f895ba8db83a4e5fbe7ef73de08
francis mwangi <43579267+francis-mwas@users.noreply.github.com>
2021-06-21 09:49:23 +0300

    Revert "Started working on rooms section"

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,13 +2,11 @@ import React from 'react'
 import Hero  from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
-import RoomContainer from '../components/RoomContainer';
 
 function Rooms() {
     return <Hero hero="roomsHero">
         <Banner title="our rooms">
         <Link to="/" className="btn-primary">return home</Link>
-        <RoomContainer />
         </Banner>
     </Hero>
 }
